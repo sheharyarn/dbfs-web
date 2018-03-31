0f364ff3e5a343043eab64b8f8f0e9450d4bccdc
Sheharyar Naseer <hello@sheharyar.me>
2018-03-31 15:49:43 -0400

    Wrap constants in a function

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,13 @@
-const constants = {
-  api: {
-    url: "http://localhost:3000/api/v1",
+const constants = (function() {
+  const host   = () => "localhost";
+  const port   = () => (window.location.port - 1000);
+  const url    = () => `http://${host()}:${port()}/api/v1`;
+
+  return {
+    api: {
+      host, port, url
+    }
   }
-}
+})();
 
 export default constants;
