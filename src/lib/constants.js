1647c5ffc4b036d41322937c3779c2ccdf4435f0
Sheharyar Naseer <hello@sheharyar.me>
2018-04-01 15:53:10 -0400

    Returning socket url in constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,10 +2,11 @@ const constants = (function() {
   const host   = () => "localhost";
   const port   = () => (window.location.port - 1000);
   const url    = () => `http://${host()}:${port()}/api/v1`;
+  const socket = () => `ws://${host()}:${port()}/socket`;
 
   return {
     api: {
-      host, port, url
+      host, port, url, socket,
     }
   }
 })();
